ca5963b1898d7917676d29a711ce99619c81a0c5
gpapadis <gpapadis@yahoo.gr>
2022-03-09 16:27:48 +0200

    Add files via upload

---
+++ nnmethods/falconn/crosspolytopeLSH/D7.py
@@ -0,0 +1,187 @@
+from __future__ import print_function
+import numpy as np
+import falconn
+import timeit
+import math
+import psutil
+
+if __name__ == '__main__':
+    datasetN = 7
+    column = 'Embedded Title'
+    datapart = 'A'
+    querypart = 'B'
+    main_dir = "/home/gap2/Documents/blockingNN/data/csvProfiles/falconn/"
+
+    for column in ['Embedded Clean Title','Embedded Clean Ag.Value']:
+     for iteration in range(0, 10):
+          print('Reading the dataset')
+          dataset = np.load(main_dir + 'D{}'.format(datasetN)+datapart+'-'+column+'.npy')
+          queries = np.load(main_dir + 'D{}'.format(datasetN)+querypart+'-'+column+'.npy')
+          IDdata = list(np.load(main_dir + 'D{}'.format(datasetN)+datapart+'ID-'+column+'.npy'))
+          IDqueries = list(np.load(main_dir + 'D{}'.format(datasetN)+querypart+'ID-'+column+'.npy'))
+          gt = np.load(main_dir + 'D{}'.format(datasetN)+'GT-'+column+'.npy')
+          print('Done')
+
+          # It's important not to use doubles, unless they are strictly necessary.
+          # If your dataset consists of doubles, convert it to floats using `astype`.
+          assert dataset.dtype == np.float32
+
+          # Normalize all the lenghts, since we care about the cosine similarity.
+          print('Normalizing the dataset')
+          dataset /= np.linalg.norm(dataset, axis=1).reshape(-1, 1)
+          queries /= np.linalg.norm(queries, axis=1).reshape(-1, 1)
+          print('Done')
+
+
+          # Perform linear scan using NumPy to get answers to the queries.
+          #print('Solving queries using linear scan')
+          #t1 = timeit.default_timer()
+          #answers = []
+          #for query in queries:
+          #    answers.append(np.dot(dataset, query).argmax())
+          #t2 = timeit.default_timer()
+          #print('Done')
+          #print('Linear scan time: {} per query'.format((t2 - t1) / float(
+          #   len(queries))))
+
+          # Center the dataset and the queries: this improves the performance of LSH quite a bit.
+          print('Centering the dataset and queries')
+          center = np.mean(dataset, axis=0)
+          dataset -= center
+          queries -= center
+          print('Done')
+
+          #params_cp = falconn.get_default_parameters(len(dataset), len(dataset[0]), falconn.DistanceFunction.EuclideanSquared, True)
+          #print('lsh family: ', params_cp.lsh_family)
+          #print('number of tables: ', params_cp.l)
+          #print('number of rotations: ', params_cp.num_rotations)
+          #print('number of hash functions: ', params_cp.k)
+          
+          # we build only 50 tables, increasing this quantity will improve the query time
+          # at a cost of slower preprocessing and larger memory footprint, feel free to
+          # play with this number
+          outstring = main_dir + "Output/D{}_CP".format(datasetN)+column+datapart+querypart+".txt"
+          out = open(outstring, 'a')
+          out.write("LSH \t dist Funct \t num tables \t num hash funct \t last cp dim \t num probes \t index time \t query time \t true matches \t all candidates \t all matches \t Index \t Query \t Indexmemory \t Searchmemory \t storage\n")
+          out.close()
+          for L in range(25,26,1):
+              for k in range(1,2):
+                  for cpdim in range(1,2):
+                      if column == "Embedded Clean Title":
+                          number_of_tables = 1
+                          cpd = 64
+                          number_of_functions = 4
+                          number_of_probes = 1
+                          storage = falconn.StorageHashTable.LinearProbingHashTable
+                      
+                      if column == "Embedded Clean Ag.Value":
+                          number_of_tables = 8
+                          cpd = 128
+                          number_of_functions = 2
+                          number_of_probes = 339
+                          storage = falconn.StorageHashTable.BitPackedFlatHashTable
+                      
+                      t1 = timeit.default_timer()
+                      #params_cp = falconn.get_default_parameters(len(dataset), len(dataset[0]), falconn.DistanceFunction.EuclideanSquared,False)
+                      params_cp = falconn.LSHConstructionParameters()
+                      params_cp.dimension = len(dataset[0])
+                      params_cp.lsh_family = falconn.LSHFamily.CrossPolytope
+                      params_cp.distance_function = falconn.DistanceFunction.EuclideanSquared #NegativeInnerProduct
+                      params_cp.l = number_of_tables
+                      # we set one rotation, since the data is dense enough,
+                      # for sparse data set it to 2
+                      params_cp.num_rotations = 1
+                      params_cp.seed = 5721840
+                      # we want to use all the available threads to set up
+                      params_cp.num_setup_threads = 0
+                      params_cp.storage_hash_table = storage
+                      # we build 18-bit hashes so that each table has
+                      # 2^18 bins; this is a good choise since 2^18 is of the same
+                      # order of magnitude as the number of data points
+                      #falconn.compute_number_of_hash_functions(num_hash_bits, params_cp)
+                      params_cp.k = number_of_functions
+                      params_cp.last_cp_dimension = cpd
+                      #print(params_cp.feature_hashing_dimension) # = 2**k
+                      
+                      
+                      print('Number of hash functions: ',params_cp.k)
+                      #print('Last cp dimension: ', params_cp.last_cp_dimension)
+
+
+                      print('Constructing the LSH table')
+                      m1= psutil.Process().memory_info().rss / (1024 * 1024)
+                      table = falconn.LSHIndex(params_cp)
+                      table.setup(dataset)
+                      m2 = psutil.Process().memory_info().rss / (1024 * 1024)
+                      t2 = timeit.default_timer()
+                      print('Done')
+                      constructtime = t2-t1
+                      indexm = m2-m1
+                      print('Construction time: {}'.format(constructtime))
+                      
+                      t1 = timeit.default_timer()
+                      query_object = table.construct_query_object()
+                      
+
+                      # find the smallest number of probes to achieve accuracy 0.9
+                      # using the binary search
+                      print('Choosing number of probes')
+                      number_of_probes = params_cp.l
+
+                      def evaluate_number_of_probes(number_of_probes):
+                          query_object.set_num_probes(number_of_probes)
+                          score = 0
+                          
+                          for i,j in gt:
+                              if IDdata.index(i) in query_object.get_unique_candidates(queries[IDqueries.index(j)]):
+                                  score +=1
+                          #for (i, query) in enumerate(queries):
+                          #    if answers[i] in query_object.get_candidates_with_duplicates(
+                          #            query):
+                          #        score += 1
+                          return float(score) / len(gt), score
+
+                      #while True:
+                      #    accuracy, score = evaluate_number_of_probes(number_of_probes)
+                      #    print('{} -> {}'.format(number_of_probes, accuracy))
+                      #    if accuracy >= 0.9:
+                      #        break
+                      #    number_of_probes = number_of_probes * 2
+                      #if number_of_probes > params_cp.l:
+                      #    left = number_of_probes // 2
+                      #    right = number_of_probes
+                      #    while right - left > 1:
+                      #        number_of_probes = (left + right) // 2
+                      #        accuracy, score = evaluate_number_of_probes(number_of_probes)
+                      #        print('{} -> {}'.format(number_of_probes, accuracy))
+                      #        if accuracy >= 0.9:
+                      #            right = number_of_probes
+                      #        else:
+                      #            left = number_of_probes
+                      #    number_of_probes = right
+                      #print('Done')
+                      #print('{} probes'.format(number_of_probes))
+
+                      # final evaluation
+                      query_object.set_num_probes(number_of_probes)
+                      m1 = psutil.Process().memory_info().rss / (1024 * 1024)
+                      finscore = 0
+                      for i,j in gt:
+                          if IDdata.index(i) in query_object.get_unique_candidates(queries[IDqueries.index(j)]):
+                              finscore +=1
+                      
+                      m2= psutil.Process().memory_info().rss / (1024 * 1024)
+                      t2 = timeit.default_timer()
+                      querym = m2-m1
+                      querytime = t2-t1
+                      print('Query time: {}'.format((querytime) / len(queries)))
+                      print('Precision: {}'.format(float(finscore) / len(gt)))
+                      
+                      #full number of candidates:
+                      fullCandidates=0
+                      for query in queries:
+                          fullCandidates += len(query_object.get_unique_candidates(query))
+                      #out.write("{} \t {}".format(num_hash_bits, number_of_tables))
+                      out = open(outstring, 'a')
+                      out.write("{} \t {} \t {} \t {} \t {} \t {} \t {} \t {} \t{} \t {} \t {} \t {} \t {} \t {} \t {} \t {} \n".format(params_cp.lsh_family, params_cp.distance_function, params_cp.l, params_cp.k, params_cp.last_cp_dimension, number_of_probes, constructtime, querytime, finscore, fullCandidates, len(gt), len(dataset), len(queries), indexm, querym, params_cp.storage_hash_table))
+                      out.close()
---
+++ nnmethods/falconn/preparefiles/D3.py
@@ -0,0 +1,117 @@
+
+#!/usr/bin/python
+
+import sys
+import struct
+import numpy as np
+import pandas as pd
+
+
+def fileread(filename, column, deliminator = '|',
+             Truthtable =False, indexpart='A', querypart='B'):
+    """
+    reads in a file and returns data from it as a list.
+    
+    MANDATORY INPUT:
+        filename (string)
+        column (string) -> which column do you want the data from
+        
+    OPTIONAL INPUT:
+        deliminator (string, default '|') -> what character separates the
+            columns in the file
+        Truthtable (boolean, default False) -> instead of a data file, read in
+            the ground truth table given as input, modifies the output
+        indexpart (string, default 'A') -> only relevant if Truthtable is True
+            which part of the dataset was used for index creation
+        querypart (string, default 'B') -> only relevant if Truthtable is True
+            which part of the dataset was used for query
+
+    OUTPUT:
+        if Truthtable is False (default): returns the data from the column as
+            list and a second list containing the IDs of the data. The third 
+            list contains the IDs of the removed entries
+        if Truthtable is True: returns two lists, first the column of the
+            indexpart, second the column of the querypart from the groundtruth 
+            file
+
+    """
+    print('reading now in file '+filename+ ', column ' + column)
+    file = pd.read_csv(filename,sep = deliminator, engine = 'python')
+    if not Truthtable:
+        rawdata = file[column]
+        emptyIDs = []
+        data = []
+        for i in range(len(rawdata)):              #strip the string of a list
+                                                 #of any unnecessary characters
+                                                 #and divide it into a list of 
+                                                 #single strings
+            vector = rawdata[i]
+            if not vector == '' and not pd.isnull(vector):
+                vector = vector.strip('][ ').replace('\n', '').replace('   ', ' ').replace('  ', ' ').split(' ')
+                for string in vector:
+                    if string == '':                 #remove empty strings
+                        vector.remove(string)
+                    else:                            #and convert the strings into
+                        string = float(string)       #floats
+                data.append(vector)
+            else:
+                emptyIDs.append(i)
+        return data, list(file['Id']), emptyIDs  #return the newly created list
+                                                 #and the IDs of the items
+                                                 #which were originally stored
+                                                 #as int
+    else:                                        #if Truthtable is True
+                                                 #return the entries from the
+                                                 #groundtruth table
+        return list(file[column+indexpart]), list(file[column+querypart])
+        
+        
+#Main infos for starting
+main_dir = '/home/gap2/Documents/blockingNN/data/csvProfiles/' #folder of the datasets and embeddings
+filenumber = 3                       #which dataset should be used
+indexpart = 'A'                      #which part of the dataset is used
+                                     #for index creating
+querypart = 'B'                      #which part of the dataset is used
+                                     #for querying
+deli = '#'                           #delimiter in the files
+
+#creating the filenames from the infos given above
+fileIndex = main_dir + 'fastText/D{}'.format(filenumber)+indexpart+'emb.csv'
+fileQuery = main_dir + 'fastText/D{}'.format(filenumber)+querypart+'emb.csv'
+filetruth = main_dir + 'D{}groundtruth.csv'.format(filenumber)
+
+
+
+#loop through all columns "Embedded Title"  "Embedded Clean Title"
+for column in ["Embedded Title", "Embedded Clean Title", "Embedded Ag.Value", "Embedded Clean Ag.Value"]:
+    
+    #reading in the files
+    EmbIndex, IDIndex, dubIDIndex = fileread(fileIndex, column, deliminator=deli)
+    EmbIndex = np.array(EmbIndex,dtype='float32')
+    IDIndex = np.array(IDIndex)
+    
+    EmbQuery, IDQuery, dubIDQuery = fileread(fileQuery, column, deliminator=deli)
+    EmbQuery = np.array(EmbQuery, dtype='float32')
+    IDQuery = np.array(IDQuery)
+    
+    truthIndex, truthQuery = fileread(filetruth, 'EntityFromPart', deli, True, indexpart, querypart)
+                                      
+    #if the ID in the truthtable starts at zero and not at the higher number
+    #as in the dataset, please uncomment these lines, otherwise leave commented
+    IDIndex = np.arange(len(IDIndex))
+    IDQuery = np.arange(len(IDQuery))
+    
+    truthtable = []
+    for i in range(len(truthIndex)):
+        if not (list(IDIndex).index(truthIndex[i]) in dubIDIndex or
+                list(IDQuery).index(truthQuery[i]) in dubIDQuery):
+            truthtable.append((truthIndex[i],truthQuery[i]))
+    IDIndex = np.delete(IDIndex, dubIDIndex,0)
+    IDQuery = np.delete(IDQuery, dubIDQuery,0)
+    
+
+    np.save(main_dir + "falconn/D{}".format(filenumber)+indexpart+"-"+column, np.array(EmbIndex))
+    np.save(main_dir + "falconn/D{}".format(filenumber)+indexpart+"ID-"+column, np.array(IDIndex))
+    np.save(main_dir + "falconn/D{}".format(filenumber)+querypart+"-"+column, np.array(EmbQuery))
+    np.save(main_dir + "falconn/D{}".format(filenumber)+querypart+"ID-"+column, np.array(IDQuery))
+    np.save(main_dir + "falconn/D{}".format(filenumber)+"GT-"+column, np.array(truthtable))
